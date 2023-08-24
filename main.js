a955504f86f6cc193965bbd85305110197fe9687
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-24 13:45:40 +0100

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+let likeBtn =  document.querySelectorAll(".like-glyph")
+
+likeBtn.forEach((addBtn)=> {
+  addBtn.addEventListener("click", ()=> {
+    mimicServerCall();
+
+    if (addBtn){
+    addBtn.classList.add("activated-heart") 
+      addBtn.textContent = FULL_HEART
+    } else{
+        addBtn.textContent = EMPTY_HEART
+    }
 
 
+  })
+})
 
+document.addEventListener("DOMContentLoaded", (e)=> {
+  e.preventDefault();
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -20,6 +38,11 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
-  });
+    }, 300)
+  }).then((data)=> {
+    console.log(data)
+    modal.classList.add("hidden")
+}).catch((err)=> {
+  console.log(err)
+});
 }
